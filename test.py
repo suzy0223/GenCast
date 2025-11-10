875ab3eb843c0ab14235c0edbccfcc1e5faf8c40
Suzy <811192022@qq.com>
2025-11-10 21:37:48 +1100

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-data = np.load('/Users/suxs3/Downloads/GenCast/code/Dataset/metr/prompt_embeddings_road_wo_address_limited_poi.npy')
-print(data.shape)
-# min max 
-print(np.min(data), np.max(data))
-# mean std
-print(np.mean(data), np.std(data))
